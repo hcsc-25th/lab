45fa5cb336ff956329c4173c781e7e44933a8f78
alsonow <75727536+samko5sam@users.noreply.github.com>
2021-07-12 18:42:49 +0800

    update new docs

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -1,4 +1,34 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: 'HCSC LAB 竹中科研25屆',
+  description: '好玩又有趣的科學知識和科學實驗在竹中科研！',
+  base: '/lab/',
+  head: [
+    ['link', { rel: 'icon', href: 'https://hcsc-25th.github.io/s/img/profilePicture-20210612-nobackground.png' }]
+  ],
+  themeConfig: {
+    logo: 'https://hcsc-25th.github.io/s/img/profilePicture-20210612-nobackground.png',
+    nav: [
+      { text: '竹中科研 主網站', link: 'https://hcsc-25th.github.io/' },
+      { text: 'HCSC BLOG', link: 'https://hcsc-25th.github.io/blog/' },
+      { text: '竹中科研 連結頁面', link: 'https://hcsc-25th.github.io/s/' },
+    ],
+    sidebar: {
+      '/experiment/': [
+        '',
+        'lab-001'
+      ],
+      '/trivia/': [
+        '',
+      ],
+      '/turning-point/': [
+        '',
+      ],
+      '/': [
+        '',
+      ]
+    },
+    sidebarDepth: 2,
+    lastUpdated: '最後Commit',
+    smoothScroll: true
+  }
 }
--- vuepress-starter/docs/README.md
+++ vuepress-starter/docs/README.md
@@ -1,1 +1,17 @@
-# Hello VuePress
+---
+home: true
+heroImage: https://hcsc-25th.github.io/s/img/profilePicture-20210612-nobackground.png
+heroText: HCSC LAB 竹中科研25屆
+tagline: 好玩又有趣的科學知識和科學實驗在竹中科研！
+actionText: 馬上看實驗 →
+actionLink: /experiment/
+features:
+- title: 科學冷知識
+  details: 科學相關的冷知識
+- title: 趣味實驗
+  details: 有趣的簡單小實驗
+- title: 科學史上重大發現
+  details: 科學史上重大發現
+footer: Copyright © 2021- HCSC25
+---
+
